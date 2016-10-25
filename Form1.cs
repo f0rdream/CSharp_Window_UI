56de8193feca335648941de5aff457ee0717790d
fordream001 <littlepigwang@outlook.com>
2016-10-25 17:37:53 +0800

    Add Student Class

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace Window_UI
 {
@@ -15,6 +16,41 @@ namespace Window_UI
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
---
+++ Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Window_UI
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public decimal ID { get; set; }
+        public string StuClass { get; set; }
+        public string School { get; set; }
+        public string Region { get; set; }
+        public DateTime StuDate { get; set; }
+        public string Health { get; set; }
+        public decimal IDCard { get; set; }
+        public decimal PostNum { get; set; }
+        public decimal PhoneNum { get; set; }
+
+        public Student(string Name, decimal ID, string StuClass, string School, string Region, DateTime StuDate,
+            string Health, decimal IDCard, decimal PostNum, decimal PhoneNum)
+        {
+            this.Name = Name;
+            this.ID = ID;
+            this.StuClass = StuClass;
+            this.School = School;
+            this.Region = Region;
+            this.StuDate = StuDate;
+            this.Health = Health;
+            this.IDCard = IDCard;
+            this.PostNum = PostNum;
+            this.PhoneNum = PhoneNum;
+        }
+    }
+}
